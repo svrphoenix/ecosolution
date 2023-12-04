1e6fcb263ee0d63313b0e5888fe5f36c91fc261b
Serhii Rudenko <svr.phoenix@gmail.com>
2023-12-04 21:44:13 +0200

    Added couner for kWh

--- src/components/ElectricitySection/ElectricitySection.jsx
+++ src/components/ElectricitySection/ElectricitySection.jsx
@@ -7,15 +7,32 @@ import {
   ElectricityTitle,
   VerticalLine,
 } from './ElectricitySection.styled';
-import { forwardRef } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 
 const ElectricitySection = forwardRef(function ElectricitySection(props, ref) {
+  const [counter, setCounter] = useState(1134147.814);
+
+  const numberWithCommas = number => {
+    const numberParts = number.toFixed(3).split('.');
+    numberParts[0] = numberParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return numberParts.join(',');
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter(prevCounter => prevCounter + 1.248);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <ElectricitySectionStyled id={props.id} ref={ref}>
       <ElectricityTitle>Electricity we produced for all time</ElectricityTitle>
       <VerticalLine />
       <Counter>
-        1.134.147.814<CounterUnit>kWh</CounterUnit>
+        {numberWithCommas(counter)}
+        <CounterUnit>kWh</CounterUnit>
       </Counter>
     </ElectricitySectionStyled>
   );
